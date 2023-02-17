012dfec2cfbe3e1caee6a82649d4cd4d1376ec9e
akshayzagad <akshayzagade62@gmail.com.com>
2023-02-17 10:52:23 +0530

    Apply validation on DTO Properties like name and salary

--- src/main/java/com/example/employee_payroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/employee_payroll/dto/EmployeePayrollDTO.java
@@ -1,5 +1,9 @@
 package com.example.employee_payroll.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,11 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public @Data class EmployeePayrollDTO {
+    @NotEmpty(message = "Please Enter your firstName")
+    @Pattern(regexp = "^[A-Z]{1}[a-z]{2,}\\s{0,}$",message = "Please enter valid name")
     public String name;
+    @Min(value = 500,message = "Minimum salary should be 500")
+    @NotNull(message = "salary field cannot be empty")
     public long salary;
 
     public EmployeePayrollDTO(String name, long salary) {
