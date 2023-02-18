da8151cd4e0b85b0214b2415f2ce9a5e6b415de4
akshayzagad <akshayzagade62@gmail.com.com>
2023-02-18 09:17:03 +0530

    Expand DTO And Model Class And Give Vallidation On each field

--- src/main/java/com/example/employee_payroll/dto/EmployeePayrollDTO.java
+++ src/main/java/com/example/employee_payroll/dto/EmployeePayrollDTO.java
@@ -1,10 +1,7 @@
 package com.example.employee_payroll.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -17,15 +14,21 @@ import java.util.List;
 @ToString
 public @Data class EmployeePayrollDTO {
     @NotEmpty(message = "Please Enter your firstName")
-    @Pattern(regexp = "^[A-Z]{1}[a-z]{3,}\\s{0,}$",message = "Please enter valid name")
+    @Pattern(regexp = "^[A-Z]{1}[a-z]{3,}$",message = "Please enter valid name")
     public String name;
     @Min(value = 500,message = "Minimum salary should be 500")
     @NotNull(message = "salary field cannot be empty")
     public long salary;
+    @Pattern(regexp = "male|female",message = "Gender may be male or female")
     public String gender;
     @JsonFormat(pattern = "dd MM yyyy")
+    @NotNull(message = "start Date should not be empty")
+    @PastOrPresent(message = "startDate should be past or today date")
     public LocalDate startDate;
+    @NotBlank(message = "Note can not be null")
     public String note;
+    @NotBlank(message = "profilePic can not be null")
     public String profilePic;
+    @NotNull(message = "department Date should not be empty")
     public List<String> department;
 }
